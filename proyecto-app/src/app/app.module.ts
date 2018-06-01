59b0a7712e166fc2ce175fdb74b200214fe3a41b
Jefferson Paredes <jefferson.paredes@finantech.com.ec>
2018-05-31 19:52:03 -0500

    Componente Login

--- proyecto-app/src/app/app.module.ts
+++ proyecto-app/src/app/app.module.ts
@@ -7,17 +7,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    CuadroLugarComponent
+    CuadroLugarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     MatCardModule,
     MatInputModule,
-    MatSelectModule, BrowserAnimationsModule
+    MatSelectModule, BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
