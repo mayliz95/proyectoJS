38d5b4ef6905e7b6dfd63cc479f029de46d38153
mayliz95 <mayra.rosero@epn.edu.ec>
2018-06-01 16:22:02 -0500

    repo funcionando

--- proyecto-app/src/app/app-routing.module.ts
+++ proyecto-app/src/app/app-routing.module.ts
@@ -1,23 +1,17 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CuadroLugarComponent} from "./cuadro-lugar/cuadro-lugar.component";
 import {LoginComponent} from "./login/login.component";
-import {AppComponent} from "./app.component";
-<<<<<<< HEAD
 import { GraficoTiempoRealComponent } from './grafico-tiempo-real/grafico-tiempo-real.component';
-=======
 import {SignupComponent} from "./signup/signup.component";
->>>>>>> 8700df1139f8a37f6e9daabb038d8ef97f60496f
+
 
 const routes: Routes = [
   { path: 'lugar', component: CuadroLugarComponent },
   { path: 'login', component: LoginComponent },
-<<<<<<< HEAD
-  { path: 'grafico', component: GraficoTiempoRealComponent}
-=======
+  { path: 'grafico', component: GraficoTiempoRealComponent},
   { path: 'signup', component: SignupComponent }
->>>>>>> 8700df1139f8a37f6e9daabb038d8ef97f60496f
-];
+  ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- proyecto-app/src/app/app.module.ts
+++ proyecto-app/src/app/app.module.ts
@@ -10,22 +10,16 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { LoginComponent } from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from "./app-routing.module";
-<<<<<<< HEAD
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { GraficoTiempoRealComponent } from './grafico-tiempo-real/grafico-tiempo-real.component';
-=======
 import { SignupComponent } from './signup/signup.component';
->>>>>>> 8700df1139f8a37f6e9daabb038d8ef97f60496f
 @NgModule({
   declarations: [
     AppComponent,
     CuadroLugarComponent,
     LoginComponent,
-<<<<<<< HEAD
-    GraficoTiempoRealComponent
-=======
+    GraficoTiempoRealComponent,
     SignupComponent
->>>>>>> 8700df1139f8a37f6e9daabb038d8ef97f60496f
   ],
   imports: [
     BrowserModule,
