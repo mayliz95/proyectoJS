3b87dc1b6b6a8b0b5e34984cc60bd3eb551e6a62
mayliz95 <mayra.rosero@epn.edu.ec>
2018-07-31 16:27:28 -0500

    Backend - entidades

---
+++ backend-base/api/models/Lugar.js
@@ -0,0 +1,20 @@
+/**
+ * Lugar.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: {
+      type: 'string',
+      required: true
+    },
+    tipolugarIdFK: {
+      model: 'Tipolugar'
+    }
+  },
+};
+
