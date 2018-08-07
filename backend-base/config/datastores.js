e5830110369a299378014fe2af2d1ba38d17015f
mayliz95 <mayra.rosero@epn.edu.ec>
2018-08-06 22:00:58 -0500

    login detalles visuales

--- backend-base/config/datastores.js
+++ backend-base/config/datastores.js
@@ -49,9 +49,9 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root:root@192.168.137.42:3306/proyectojs',
+    url: 'mysql://root:root@localhost:3306/proyectojs',
     //host: 'aplicacioneswebjs.mysql.database.azure.com:3306',
-    insecureAuth: true,
+    //insecureAuth: true,
     //url: 'mysql://jeffersoinpm@aplicacioneswebjs:Aplicacionesweb123@aplicacioneswebjs.mysql.database.azure.com:3306/js-base',
   },
 };
