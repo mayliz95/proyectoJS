8efb73a5f2ab4056509cb4e58a5217ffe7225696
mayliz95 <mayra.rosero@epn.edu.ec>
2018-08-08 10:13:20 -0500

    --

--- proyecto-app/src/app/tiempo-real/tiempo-real.component.ts
+++ proyecto-app/src/app/tiempo-real/tiempo-real.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import {TemperaturaService} from "../Servicios/temperatura.service";
+import {Temperatura} from "../interfaces/temperatura";
 
 @Component({
   selector: 'app-tiempo-real',
   templateUrl: './tiempo-real.component.html',
-  styleUrls: ['./tiempo-real.component.css']
+  styleUrls: ['./tiempo-real.component.css'],
+  providers: [TemperaturaService]
 })
 export class TiempoRealComponent implements OnInit {
 
   private intervalUpdate: any = null;
   public chart: any = null;
-
-  constructor() { }
+  temperaturas: Temperatura[];
+  constructor(private temperaturaService: TemperaturaService) { }
 
   ngOnInit() {
     this.intervalUpdate = setInterval(function(){
@@ -63,5 +66,30 @@ export class TiempoRealComponent implements OnInit {
   }
   private showData(): void {
     console.log(1);
+    let valTemp = [];
+    let fechaLabels = [];
+    this.temperaturaService.getTemperaturaPorIdDispositivoLugar(1).subscribe(
+      (response:any) => {
+        this.temperaturas = response;
+        this.temperaturas.forEach(function (element) {
+            //let fecha = new Date(element.fecha);
+            let chartTime: any = new Date(response);
+            chartTime = chartTime.getHours() + ':' + ((chartTime.getMinutes() < 10) ? '0' + chartTime.getMinutes() : chartTime.getMinutes()) + ':' + ((chartTime.getSeconds() < 10) ? '0' + chartTime.getSeconds() : chartTime.getSeconds());
+            if(fechaLabels.length > 15) {
+              fechaLabels.shift();
+              valTemp.shift();
+            }
+            fechaLabels.push(chartTime);
+            valTemp.push(element.valorTemperatura);
+            //fechaTemp.push(element.fecha.replace("T","\n"));
+          }
+        );
+
+        this.chart.data.labels.push(fechaLabels);
+        this.chart.data.datasets[0].data.push(valTemp);
+        this.chart.update();
+    }, error => {
+      console.error("ERROR: Unexpected response");
+    });
   }
 }
